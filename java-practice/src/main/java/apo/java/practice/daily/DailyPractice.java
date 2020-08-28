f002e8392ee3fabbef1c59d8ea918274f99b3c76
apobits <apobits@gmail.com>
2020-08-28 14:31:17 -0500

    updated

--- java-practice/src/main/java/apo/java/practice/daily/DailyPractice.java
+++ java-practice/src/main/java/apo/java/practice/daily/DailyPractice.java
@@ -1,12 +1,14 @@
 package apo.java.practice.daily;
 
+import javax.swing.*;
+
 public class DailyPractice {
 
     public static void main(String[] args) {
 
+        JOptionPane.showMessageDialog(null,"You can do it");
+        System.out.println("Working in linux!!!");
 
     }
 
 }
-
-
--- java-practice/src/main/java/module-info.java
+++ java-practice/src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module apo.java.practice {
     exports apo.java.practice.daily;
     requires junit;
+    requires java.desktop;
 }
