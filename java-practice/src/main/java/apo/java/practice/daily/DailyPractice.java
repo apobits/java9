bdabf1061410c1b93817a240897796d195154d1d
apobits <apobits@gmail.com>
2020-10-13 03:29:32 -0500

    updated

--- java-practice/src/main/java/apo/java/practice/daily/DailyPractice.java
+++ java-practice/src/main/java/apo/java/practice/daily/DailyPractice.java
@@ -1,13 +1,16 @@
 package apo.java.practice.daily;
 
 public class DailyPractice {
+
     public static void main(String[] args) {
-	byte b = Byte.MAX_VALUE;
-	short s = Short.MAX_VALUE;
-	int i = Integer.MAX_VALUE;
-	long l = Long.MAX_VALUE;
-	float f = Float.MAX_VALUE;
-	double d = Double.MAX_VALUE;
-	System.out.println("FUck");
+        byte b = Byte.MAX_VALUE;
+        short s = Short.MAX_VALUE;
+        int i = Integer.MAX_VALUE;
+        long l = Long.MAX_VALUE;
+        float f = Float.MAX_VALUE;
+        double d = Double.MAX_VALUE;
+
+        var a = new int[10];
+        System.out.println(a.length);
     }
 }
